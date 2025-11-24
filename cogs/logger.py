d3230653d730123c40f25d5cdfe3b73dda37777b
Noderyos <vincent.bsod@gmail.com>
2025-11-24 19:11:30 +0100

    Fix archived thread when logging

--- cogs/logger.py
+++ cogs/logger.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 from config_loader import config
 import utils
-
+import discord
 
 class Logger(commands.Cog, utils.Logger):
     def __init__(self, bot: commands.Bot):
@@ -13,36 +13,52 @@ class Logger(commands.Cog, utils.Logger):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        log_channel = self.bot.get_channel(config.LOG_MEMBERS_CHANNEL_ID)
-        if log_channel:
-            await log_channel.send(f":door: **{member.name}** has joined.")
+        await self.send_message(
+            config.LOG_MEMBERS_CHANNEL_ID,
+            f":door: **{member.name}** has joined."
+        )
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        log_channel = self.bot.get_channel(config.LOG_MEMBERS_CHANNEL_ID)
-        if log_channel:
-            await log_channel.send(f":wave: **{member.name}** has left.")
+        await self.send_message(
+            config.LOG_MEMBERS_CHANNEL_ID,
+            f":wave: **{member.name}** has left."
+        )
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
-        log_channel = self.bot.get_channel(config.LOG_MESSAGES_CHANNEL_ID)
-        if log_channel:
-            author = message.author
-            content = message.content or "[Empty or non-textual message]"
-            await log_channel.send(
-                f":wastebasket: **{author}** deleted a message in **{message.channel}**:\n" +
-                f"```\n{content}\n```"
-            )
+        author = message.author
+        content = message.content or "[Empty or non-textual message]"
+
+        await self.send_message(
+            config.LOG_MESSAGES_CHANNEL_ID,
+            f":wastebasket: **{author}** deleted a message in **{message.channel}**:\n"
+            f"```\n{content}\n```"
+        )
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
-        log_channel = self.bot.get_channel(config.LOG_MESSAGES_CHANNEL_ID)
-        if log_channel and before.content != after.content:
-            await log_channel.send(
-                f":pencil2: **{before.author}** edited a message in **{before.channel}**:\n" +
-                f"**Before:**\n```\n{before.content}\n```\n**After:**\n```\n{after.content}\n```"
-            )
+        if before.content == after.content:
+            return
 
+        await self.send_message(
+            config.LOG_MESSAGES_CHANNEL_ID,
+            f":pencil2: **{before.author}** edited a message in **{before.channel}**:\n"
+            f"**Before:**\n```\n{before.content}\n```"
+            f"**After:**\n```\n{after.content}\n```"
+        )
+    
+    async def send_message(self, channel_or_thread_id, content):
+        channel = await self.bot.fetch_channel(channel_or_thread_id)
+
+        if isinstance(channel, (discord.Thread,)):
+            if channel.archived:
+                await channel.edit(archived=False)
+            return await channel.send(content)
+        elif isinstance(channel, discord.TextChannel):
+            return await channel.send(content)
+        else:
+            self.error(f"Channel type {type(channel)} not supported")
 
 async def setup(bot: commands.Bot):
     cog = Logger(bot)
